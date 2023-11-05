ee4a2ac86fe4fa1e41e3b4991a549b9ee2541677
Wafi <wafiakmal@gmail.com>
2023-11-04 23:29:50 -0400

    add first page

--- first_thing.py
+++ first_thing.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+st.set_page_config(layout="wide")
 st.title("Base Facial Features")
 
 st.markdown(
@@ -38,9 +39,37 @@ def app():
     col_1, col_2 = st.columns(2)
     
     with col_1:
-        prompt_sex = st.text_input('Sex', placeholder='man, woman, .etc')
-        prompt_age = st.text_input('Age', placeholder='age approximations, 20ish, 30ish, .etc')
-        prompt_face_structure = st.text_input('Face Structure', placeholder='round, square, .etc')
-        prompt_eyes = st.text_input('Eyes', placeholder='big, small, .etc')
-        prompt_nose = st.text_input('Nose Shape', placeholder='big, small, .etc')
-        prompt_mouth_lips = st.text_input('Mouth and Lips', placeholder='big, small, .etc')
+
+        col_x, col_y = st.columns(2)
+        with col_x:
+            prompt_sex = st.text_input('Sex', placeholder='man, woman, .etc')
+            prompt_age = st.text_input('Age', placeholder='age approximations, 20ish, 30ish, .etc')
+            prompt_face_structure = st.text_input('Face Structure', placeholder='round, square, .etc')
+        with col_y:
+            prompt_eyes = st.text_input('Eyes', placeholder='big, small, .etc')
+            prompt_nose = st.text_input('Nose Shape', placeholder='big, small, .etc')
+            prompt_mouth_lips = st.text_input('Mouth and Lips', placeholder='big, small, .etc')
+
+    if st.button("Generate Sketch"):
+        if prompt_sex == "man":
+            dalle_prompt = f"""
+                Create an image of a {prompt_sex} in his {prompt_age} with a 
+                {prompt_face_structure} face structure. He has {prompt_eyes} 
+                eyes, a {prompt_nose} nose, and {prompt_mouth_lips} mouth and 
+                lips.
+            """
+        elif prompt_sex == "woman":
+            dalle_prompt = f"""
+                Create an image of a {prompt_sex} in her {prompt_age} with a 
+                {prompt_face_structure} face structure. Se has {prompt_eyes} 
+                eyes, a {prompt_nose} nose, and {prompt_mouth_lips} mouth and 
+                lips.
+            """
+        
+        with col_2:
+            st.text("The prompt is:")
+            st.write(dalle_prompt)
+            st.image(generate_image(dalle_prompt))
+
+if __name__ == "__main__":
+    app()
